56d8898b0eef1e845a4068febd9be0fb0f1f7986
Werwllleh <allvxm4@gmail.com>
2022-10-11 12:30:32 +0300

    1

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,11 +2,36 @@ import React from 'react';
 import './Form.css';
 
 const Form = () => {
+
+	const [name, setName] = useState('');
+	const [car, setCar] = useState('');
+	const [carNum, setCarNum] = useState('');
+	const [carNote, setCarNote] = useState('');
+
+	const onChangeName = (e) => {
+        setCountry(e.target.value)
+	}
+
+	const onChangeCar = (e) => {
+			setStreet(e.target.value)
+	}
+
+	const onChangeCarNum = (e) => {
+			setSubject(e.target.value)
+	}
+	
+	const onChangeCarNote = (e) => {
+        setSubject(e.target.value)
+	}
+
+
 	return (
 		<div className={'form'}>
-			<input className={'input'} type="text" />
-			<input className={'input'} type="text" />
-			<input className={'input'} type="text" />
+			<h1 className={'title'}>Регистрация</h1>
+			<input className={'input'} onChange={onChangeName} type="text" placeholder={'Имя и фамилия*'}/>
+			<input className={'input'} onChange={onChangeCar} type="text" placeholder={'Марка и модель авто*'} />
+			<input className={'input'} onChange={onChangeCarNum} type="text" placeholder={'Номер вашего авто*'}/>
+			<input className={'input'} onChange={onChangeCarNote} type="text" placeholder={'Примечание к авто'}/>
 		</div>
 	);
 };
