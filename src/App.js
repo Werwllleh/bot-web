518c683652a0d4a0f95722fc6437a92b2d90eac8
Werwllleh <allvxm4@gmail.com>
2022-10-11 14:14:33 +0300

    1

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header/Header';
 import Form from './components/Form/Form';
 
 import { Route, Routes } from 'react-router-dom';
+import Cars from './components/Cars/Cars';
 
 
 
@@ -20,7 +21,7 @@ function App() {
     <div className="App">
       <Header />
       <Routes>
-        <Route index element={<Form />} />
+        <Route index element={<Cars />} />
         <Route path={'form'} element={<Form />} />
       </Routes>
     </div>
