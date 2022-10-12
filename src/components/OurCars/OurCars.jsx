f477bb5a3892e0d1ff3f26e660af1ac57902f854
Werwllleh <allvxm4@gmail.com>
2022-10-12 10:54:41 +0300

    1

---
+++ src/components/OurCars/OurCars.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function OurCars() {
+	return (
+		<div>OurCars</div>
+	)
+}
+
+export default OurCars
---
+++ src/components/SearchCar/SearchCar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SearchCar() {
+	return (
+		<div>SearchCar</div>
+	)
+}
+
+export default SearchCar
