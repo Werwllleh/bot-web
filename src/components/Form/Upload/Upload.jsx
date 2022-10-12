dc25fb5656f1049c30f4c7026a7cb274a31ea00b
Werwllleh <allvxm4@gmail.com>
2022-10-12 15:09:39 +0300

    1

--- src/components/Form/Upload/Upload.jsx
+++ src/components/Form/Upload/Upload.jsx
@@ -1,80 +1,32 @@
-import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
-import { message, Upload } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
+import { Button, message, Upload } from 'antd';
 import React, { useState } from 'react';
 import 'antd/dist/antd.min.css';
 
-const getBase64 = (img, callback) => {
-  const reader = new FileReader();
-  reader.addEventListener('load', () => callback(reader.result));
-  reader.readAsDataURL(img);
-};
+const props = {
+  name: 'file',
+  action: 'https://localhost/upload',
+  headers: {
+    authorization: 'authorization-text',
+  },
 
-const beforeUpload = (file) => {
-  const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png' || file.type === 'image/heic' || file.type === 'image/heif';
-
-  if (!isJpgOrPng) {
-    message.error('Только формат изображения!');
-  }
-
-  const isLt2M = file.size / 1024 / 1024 < 2;
-
-  if (!isLt2M) {
-    message.error('Image must smaller than 2MB!');
-  }
-
-  return isJpgOrPng && isLt2M;
-};
-
-const UploadForm = () => {
-  const [loading, setLoading] = useState(false);
-  const [imageUrl, setImageUrl] = useState();
-
-  const handleChange = (info) => {
-    if (info.file.status === 'uploading') {
-      setLoading(true);
-      return;
+  onChange(info) {
+    if (info.file.status !== 'uploading') {
+      console.log(info.file, info.fileList);
     }
 
     if (info.file.status === 'done') {
-      // Get this url from response in real world.
-      getBase64(info.file.originFileObj, (url) => {
-        setLoading(false);
-        setImageUrl(url);
-      });
+      message.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
     }
-  };
-
-  const uploadButton = (
-    <div>
-      {loading ? <LoadingOutlined /> : <PlusOutlined />}
-      <div className={'upload-text'}>
-        Загрузить фото
-      </div>
-    </div>
-  );
-  return (
-    <Upload
-      name="avatar"
-      listType="picture-card"
-      className="avatar-uploader"
-      showUploadList={false}
-      action="https://193.164.149.140/bot-back/img/users_cars"
-      beforeUpload={beforeUpload}
-      onChange={handleChange}
-    >
-      {imageUrl ? (
-        <img
-          src={imageUrl}
-          alt="avatar"
-          style={{
-            width: '100%',
-          }}
-        />
-      ) : (
-        uploadButton
-      )}
-    </Upload>
-  );
+  },
 };
+
+const UploadForm = () => (
+  <Upload {...props}>
+    <Button icon={<UploadOutlined />}>Загрузить фото</Button>
+  </Upload>
+);
 
 export default UploadForm;
