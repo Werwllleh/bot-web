666a631c2bbcbc70514664a3fa4f70d617303abe
Werwllleh <allvxm4@gmail.com>
2022-10-12 16:03:11 +0300

    1

--- src/components/Form/Upload/Upload.jsx
+++ src/components/Form/Upload/Upload.jsx
@@ -1,32 +1,74 @@
-import { UploadOutlined } from '@ant-design/icons';
-import { Button, message, Upload } from 'antd';
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
+import { message, Upload } from 'antd';
 import React, { useState } from 'react';
 import 'antd/dist/antd.min.css';
 
-const props = {
-  name: 'file',
-  action: 'https://193.164.149.140/upload',
-  headers: {
-    authorization: 'authorization-text',
-  },
+const getBase64 = (img, callback) => {
+  const reader = new FileReader();
+  reader.addEventListener('load', () => callback(reader.result));
+  reader.readAsDataURL(img);
+};
 
-  onChange(info) {
-    if (info.file.status !== 'uploading') {
-      console.log(info.file, info.fileList);
+const beforeUpload = (file) => {
+  const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png' || file.type === 'image/heic' || file.type === 'image/heif';
+
+  if (!isJpgOrPng) {
+    message.error('Только формат изображения!');
+  }
+
+  return isJpgOrPng;
+};
+
+const UploadForm = () => {
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState();
+
+  const handleChange = (info) => {
+    if (info.file.status === 'uploading') {
+      setLoading(true);
+      return;
     }
 
     if (info.file.status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully`);
-    } else if (info.file.status === 'error') {
-      message.error(`${info.file.name} file upload failed.`);
+      // Get this url from response in real world.
+      getBase64(info.file.originFileObj, (url) => {
+        setLoading(false);
+        setImageUrl(url);
+      });
     }
-  },
-};
+  };
 
-const UploadForm = () => (
-  <Upload {...props}>
-    <Button icon={<UploadOutlined />}>Загрузить фото</Button>
-  </Upload>
-);
+  const uploadButton = (
+    <div>
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div className={'upload-text'}>
+        Загрузить фото
+      </div>
+    </div>
+  );
+  return (
+    <Upload
+      name="avatar"
+      listType="picture-card"
+      className="avatar-uploader"
+      showUploadList={false}
+      action="https://193.164.149.140/upload"
+      beforeUpload={beforeUpload}
+      onChange={handleChange}
+    >
+      {imageUrl ? (
+        <img
+          src={imageUrl}
+          alt="avatar"
+          style={{
+            width: '100%',
+          }}
+        />
+      ) : (
+        uploadButton
+      )}
+    </Upload>
+  );
+};
 
 export default UploadForm;
