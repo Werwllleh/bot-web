47d5d53ec4a1ab0e571a7fddc283b701f74283e5
Werwllleh <allvxm4@gmail.com>
2022-10-13 13:03:41 +0300

    1

--- src/components/Form/Upload/Upload.jsx
+++ src/components/Form/Upload/Upload.jsx
@@ -79,69 +79,23 @@ const UploadForm = () => {
 
 export default UploadForm; */
 
-import { PlusOutlined } from '@ant-design/icons';
-import { Modal, Upload } from 'antd';
-import React, { useState } from 'react';
+import { UploadOutlined } from '@ant-design/icons';
+import { Button, Upload } from 'antd';
+import 'antd/dist/antd.min.css';
+import React from 'react';
 
-const getBase64 = (file) =>
-  new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = (error) => reject(error);
-  });
-const UploadForm = () => {
-  const [previewOpen, setPreviewOpen] = useState(false);
-  const [previewImage, setPreviewImage] = useState('');
-  const [previewTitle, setPreviewTitle] = useState('');
-  const [fileList, setFileList] = useState([]);
-  const handleCancel = () => setPreviewOpen(false);
-
-  const handlePreview = async (file) => {
-    if (!file.url && !file.preview) {
-      file.preview = await getBase64(file.originFileObj);
-    }
-    setPreviewImage(file.url || file.preview);
-    setPreviewOpen(true);
-    setPreviewTitle(file.name || file.url.substring(file.url.lastIndexOf('/') + 1));
-  };
-  const handleChange = ({ fileList: newFileList }) => setFileList(newFileList);
-
-  const uploadButton = (
-    <div>
-      <PlusOutlined />
-      <div
-        style={{
-          marginTop: 8,
-        }}
-      >
-        Загрузить фото
-      </div>
-    </div>
-  );
-  return (
-    <>
-      <Upload
-        multiple={false}
-        maxCount={1}
-        action="https://193.164.149.140/api/upload"
-        listType="picture-card"
-        fileList={fileList}
-        onPreview={handlePreview}
-        onChange={handleChange}
-      >
-        {fileList.length >= 8 ? null : uploadButton}
-      </Upload>
-      <Modal open={previewOpen} title={previewTitle} footer={null} onCancel={handleCancel}>
-        <img
-          alt="example"
-          style={{
-            width: '100%',
-          }}
-          src={previewImage}
-        />
-      </Modal>
-    </>
-  );
-};
+const UploadForm = () => (
+  <>
+    <Upload
+      action="https://193.164.149.140/api/upload"
+      listType="picture"
+      showUploadList={false}
+      multiple={false}
+      maxCount={1}
+      /* defaultFileList={[...fileList]} */
+    >
+      <Button icon={<UploadOutlined />}>Upload</Button>
+    </Upload>
+  </>
+);
 export default UploadForm;
