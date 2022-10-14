e3c3c1448b9939016cd1b60c28cbc093679673f4
Werwllleh <allvxm4@gmail.com>
2022-10-14 15:28:31 +0300

    1

--- src/components/Form/Upload/Upload.jsx
+++ src/components/Form/Upload/Upload.jsx
@@ -1,86 +1,3 @@
-/* import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
-import { message, Upload } from 'antd';
-import React, { useState } from 'react';
-import 'antd/dist/antd.min.css';
-
-const getBase64 = (img, callback) => {
-  const reader = new FileReader();
-  reader.addEventListener('load', () => callback(reader.result));
-  reader.readAsDataURL(img);
-};
-
-const beforeUpload = (file) => {
-  const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/jpg' || file.type === 'image/png' || file.type === 'image/heic' || file.type === 'image/heif';
-
-  if (!isJpgOrPng) {
-    message.error('Только изображение!');
-  } else {
-    message.error('Изображение загружено');
-  }
-
-  return isJpgOrPng;
-};
-
-const UploadForm = () => {
-  const [loading, setLoading] = useState(false);
-  const [imageUrl, setImageUrl] = useState();
-
-  const handleChange = (info) => {
-
-    let newFileList = [...info.fileList];
-    newFileList = newFileList.slice(-1);
-
-    if (info.file.status === 'uploading') {
-      setLoading(true);
-      return;
-    }
-
-    if (info.file.status === 'done') {
-      // Get this url from response in real world.
-      getBase64(info.file.originFileObj, (url) => {
-        setLoading(false);
-        setImageUrl(url);
-      });
-    }
-  };
-
-  const uploadButton = (
-    <div>
-      {loading ? <LoadingOutlined /> : <PlusOutlined />}
-      <div className={'upload-text'}>
-        Загрузить фото
-      </div>
-    </div>
-  );
-  return (
-    <Upload
-      name="avatar"
-      listType="picture-card"
-      className="avatar-uploader"
-      showUploadList={false}
-      multiple={false}
-      maxCount={1}
-      action="https://193.164.149.140/api/upload"
-      beforeUpload={beforeUpload}
-      onChange={handleChange}
-    >
-      {imageUrl ? (
-        <img
-          src={imageUrl}
-          alt="avatar"
-          style={{
-            width: '100%',
-          }}
-        />
-      ) : (
-        uploadButton
-      )}
-    </Upload>
-  );
-};
-
-export default UploadForm; */
-
 import { UploadOutlined } from '@ant-design/icons';
 import { Button, Space, message, Upload } from 'antd';
 import React, { useState } from 'react';
@@ -130,16 +47,16 @@ const UploadForm = () => {
       });
     }
 
-    if (info.file.status === 'removed') {
-      // Get this url from response in real world.
-      getBase64(info.file.originFileObj, (url) => {
-        setLoading(false);
-      });
-      console.log('delete');
-    }
-
     console.log(info);
   };
+
+  const handleRemove = (info) => {
+      // info.file.status = "removed";
+      axios.post('https://193.164.149.140/api/remove', {
+        file: info.file.response,
+      });
+      console.log(info.file);
+    }
 
   return (
   <Space
@@ -154,8 +71,9 @@ const UploadForm = () => {
       maxCount={1}
       beforeUpload={beforeUpload}
       onChange={handleChange}
+      onRemove={handleRemove}
     >
-      <Button icon={<UploadOutlined />}>Загрузить фото</Button>
+      <Button icon={<UploadOutlined />}>Загрузить фото автомобиля</Button>
     </Upload>
   </Space>
   )
