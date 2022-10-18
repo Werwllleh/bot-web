a8f28cabcbd658c68b09f0ba7978861649a27d0e
Werwllleh <allvxm4@gmail.com>
2022-10-18 17:12:15 +0300

    1

--- src/components/SearchCar/SearchCar.jsx
+++ src/components/SearchCar/SearchCar.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react';
+import { useTelegram } from '../../hooks/useTelegram';
+import './SearchCar.css';
 
-function SearchCar() {
+
+const SearchCar = () => {
+
+	const [searcheble, setSearcheble] = useState('');
+	const { tg } = useTelegram();
+
+
+	useEffect(() => {
+		tg.expand()
+	}, [])
+
+	const onSearcheble = (e) => {
+		setSearcheble(e.target.value)
+	}
+
 	return (
-		<div>Поиск авто</div>
+		<div className={'searchCar'}>
+			<h1 className={'title'}>Поиск авто</h1>
+			<input className={'input'} value={searcheble} onChange={onSearcheble} type="text" placeholder={'Введите номер автомобиля'}/>
+		</div>
 	)
 }
 
