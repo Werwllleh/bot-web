c952f8a8cd0849fcc5759edc3a0615c550ceebbb
Werwllleh <allvxm4@gmail.com>
2022-10-18 18:08:30 +0300

    1

--- src/components/SearchCar/SearchCar.jsx
+++ src/components/SearchCar/SearchCar.jsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { useTelegram } from '../../hooks/useTelegram';
 import './SearchCar.css';
+import '../../App.css';
 
 
 const SearchCar = () => {
@@ -8,21 +9,39 @@ const SearchCar = () => {
 	const [searcheble, setSearcheble] = useState('');
 	const { tg } = useTelegram();
 
-
 	useEffect(() => {
 		tg.expand()
 	}, [])
 
 	const onSearcheble = (e) => {
-		setSearcheble(e.target.value)
+		setSearcheble(e.target.value.toUpperCase())
 	}
 
 	return (
 		<div className={'searchCar'}>
 			<h1 className={'title'}>Поиск авто</h1>
-			<input className={'input'} value={searcheble} onChange={onSearcheble} type="text" placeholder={'Введите номер автомобиля'}/>
+			<input className={'input-search'} value={searcheble} onChange={onSearcheble} type="text" placeholder={'Введите номер автомобиля'} />
+			<div className={'foundBody'}>
+				<div className={'foundCarImg'}>
+					<img src="https://193.164.149.140/api/image/446012794.jpeg" alt="" />
+				</div>
+				<div className={'list'}>
+					<div className={'list-right'}>
+						<div className={'list-item'}>Авто:</div>
+						<div className={'list-item'}>Владелец:</div>
+						<div className={'list-item'}>Год авто:</div>
+						<div className={'list-item'}>Примечания:</div>
+					</div>
+					<div className={'list-left'}>
+						<div className={'list-item list-item-r'}>Какое-то</div>
+						<div className={'list-item list-item-r'}>Кто-то</div>
+						<div className={'list-item list-item-r'}>Когда-то</div>
+						<div className={'list-item list-item-r'}>Какие-то</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
 
-export default SearchCar
+export default SearchCar;
