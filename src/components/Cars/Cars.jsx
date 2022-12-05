aba880617e70f3647733eb31b2febd46283d6aad
Werw <allvxm4@gmail.com>
2022-12-05 23:22:19 +0300

    1

--- src/components/Cars/Cars.jsx
+++ src/components/Cars/Cars.jsx
@@ -2,11 +2,13 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useTelegram } from '../../hooks/useTelegram';
 import s from './Cars.module.css';
+import { Image } from 'antd';
 
 
 const Cars = () => {
 
-	const [carPhotos, setCarPhotos] = useState([]);
+	const [images, setImages] = React.useState([]);
+	const [loaded, setIsLoaded] = React.useState(false);
 
 	const { tg } = useTelegram();
 
@@ -14,22 +16,37 @@ const Cars = () => {
 		tg.expand()
 	}, [])
 
+	const fetchImages = async () => {
+    await axios.get(`https://193.164.149.140/api/ourcars`)
+      .then (res => {
+        setImages([...images, ...res.data]);
+        setIsLoaded(true);
+      });
+	};
+	
 	useEffect(() => {
-		axios.get(`https://193.164.149.140/api/ourcars`)
-			.then((res) => {
-				setCarPhotos(res.data)
-		})
-	}, [])
+			fetchImages();
+	}, []);
 
 
 
 	return (
 		<div className={s.cars_body}>
-			<div className={s.cars_cards}>
-				<div className={s.cars_card}>
-					{carPhotos.map((photo) => {
-						return (<img src={"https://193.164.149.140/api/image/" + photo} alt="" />)
-					})}
+			<h1 className={s.title}>Авто нашего клуба</h1>
+			<div className={s.image_grid}>
+				<div className={s.image_item}>
+					<Image.PreviewGroup>
+						{images.map((photo) => {
+						return (
+							<Image
+								width={300}
+								key={String(photo).length+7}
+								src={"https://193.164.149.140/api/image/" + photo}
+								alt={"car" + String(photo).length + 7}
+							/>
+						)
+						})}
+						</Image.PreviewGroup>
 				</div>
 			</div>
 		</div>
