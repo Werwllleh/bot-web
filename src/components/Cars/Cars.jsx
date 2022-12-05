d5c02276f1584d428ef26e45cdc71d2d3550ea24
Werwllleh <allvxm4@gmail.com>
2022-12-05 17:59:39 +0300

    1

--- src/components/Cars/Cars.jsx
+++ src/components/Cars/Cars.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useTelegram } from '../../hooks/useTelegram';
+import s from './Cars.module.css';
+
 
 const Cars = () => {
+
+	const [carPhotos, setCarPhotos] = useState('')
+
+	const { tg } = useTelegram();
+
+	useEffect(() => { 
+		tg.expand()
+	}, [])
+
+	useEffect(() => {
+		axios.get(`https://193.164.149.140/api/ourcars`)
+			.then((res) => {
+				let arrPhoto = [];
+				setCarPhotos(arrPhoto.push(res.data))
+
+				console.log(arrPhoto);
+		})
+	}, [carPhotos])
+
+
+
 	return (
-		<div>
-			CARS
+		<div className={s.cars_body}>
+			<div className={s.cars_cards}>
+				<div className={s.cars_card}>
+					<img src={"https://193.164.149.140/api/image/" + carPhotos} alt="" />
+				</div>
+			</div>
 		</div>
 	);
 };
