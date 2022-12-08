49bc113ad68c11dc9107b3871bc66869842d2f50
Werwllleh <allvxm4@gmail.com>
2022-12-08 14:41:45 +0300

    1

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -11,6 +11,16 @@ const text = `
   it can be found as a welcome guest in many households across the world.
 `;
 
+import { Carousel } from 'antd';
+const contentStyle = {
+  margin: 0,
+  height: '160px',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+  background: '#364d79',
+};
+
 
 const Partners = () => {
 
@@ -33,19 +43,62 @@ const Partners = () => {
 
 	}, []);
 
+	const onChange = (currentSlide) => {
+    console.log(currentSlide);
+  };
+
 	return (
 		<div className={s.partners_body}>
 			<h1 className={s.title}>Партнеры клуба</h1>
 			<div className={s.content_body}>
 				<Collapse accordion>
 					<Panel header="This is panel header 1" key="1">
-						<p>{text}</p>
+						<Carousel afterChange={onChange}>
+							<div>
+								<h3 style={contentStyle}>1</h3>
+							</div>
+							<div>
+								<h3 style={contentStyle}>2</h3>
+							</div>
+							<div>
+								<h3 style={contentStyle}>3</h3>
+							</div>
+							<div>
+								<h3 style={contentStyle}>4</h3>
+							</div>
+						</Carousel>
 					</Panel>
 					<Panel header="This is panel header 2" key="2">
-						<p>{text}</p>
+						<Carousel afterChange={onChange}>
+							<div>
+								<h3 style={contentStyle}>1</h3>
+							</div>
+							<div>
+								<h3 style={contentStyle}>2</h3>
+							</div>
+							<div>
+								<h3 style={contentStyle}>3</h3>
+							</div>
+							<div>
+								<h3 style={contentStyle}>4</h3>
+							</div>
+						</Carousel>
 					</Panel>
 					<Panel header="This is panel header 3" key="3">
-						<p>{text}</p>
+						<Carousel afterChange={onChange}>
+							<div>
+								<h3 style={contentStyle}>1</h3>
+							</div>
+							<div>
+								<h3 style={contentStyle}>2</h3>
+							</div>
+							<div>
+								<h3 style={contentStyle}>3</h3>
+							</div>
+							<div>
+								<h3 style={contentStyle}>4</h3>
+							</div>
+						</Carousel>
 					</Panel>
 				</Collapse>
 			</div>
