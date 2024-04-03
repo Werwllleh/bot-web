152c631b87e3ecfff6a78c964845a5dafd5f8a98
Werwllleh <allvxm4@gmail.com>
2024-04-03 23:57:01 +0300

    order stage 4.5

--- src/components/Pages/Cart/Cart.jsx
+++ src/components/Pages/Cart/Cart.jsx
@@ -3,7 +3,9 @@ import s from './Cart.module.css';
 import Header from "../../Header/Header";
 import {checkAvailable, getTotalSumCart} from "../../../utils/cartUtils";
 import CartItem from "../../CartItem/CartItem";
-import {Empty, Select} from "antd";
+import {Empty, Select, Input, Modal} from "antd";
+const { TextArea } = Input;
+import InputMask from 'react-input-mask';
 import {SITE} from "../../../utils/consts";
 import axios from "axios";
 import {useProductsCountStore, useUsersStore} from "../../../services/store";
@@ -11,17 +13,21 @@ import {useProductsCountStore, useUsersStore} from "../../../services/store";
 const Cart = ({cart}) => {
 
   const currentUser = useUsersStore((state) => state.currentUser);
-
+  const productStore = useProductsCountStore((state) => state.productStore);
   const selectedPlace = useUsersStore((state) => state.selectedPlace);
-  const updateSelectedPlace = useUsersStore((state) => state.updateSelectedPlace);
-
   const available = useUsersStore((state) => state.available);
+
+  const updateSelectedPlace = useUsersStore((state) => state.updateSelectedPlace);
   const updateAvailable = useUsersStore((state) => state.updateAvailableProducts);
 
 
   const [requested, setRequested] = useState(false);
+  const [orderComment, setOrderComment] = useState('');
 
-  const productStore = useProductsCountStore((state) => state.productStore);
+  const [phone, setPhone] = useState('');
+  const handleInputPhone = ({ target: { value } }) => setPhone(value);
+
+
 
   useEffect(() => {
     updateAvailable(checkAvailable(productStore, cart, selectedPlace))
@@ -33,15 +39,31 @@ const Cart = ({cart}) => {
     updateAvailable(checkAvailable(productStore, cart, value))
   };
 
+  const textAreaChange = (e) => {
+    setOrderComment(e.currentTarget.value);
+  };
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
   const sendOrderData = () => {
     if (currentUser.id && selectedPlace) {
       axios
         .post(SITE + `api/order`, {
           orderData: {
+            phone: phone,
             user: currentUser,
             cart: cart,
             selectedPlace: selectedPlace,
+            comment: orderComment.length && orderComment,
             cartTotalCount: getTotalSumCart(cart).totalCount,
             cartTotalSum: getTotalSumCart(cart).totalSum,
           },
@@ -57,6 +79,7 @@ const Cart = ({cart}) => {
 
 
   return (
+    <>
     <div className={s.cart__body}>
       <Header title={"Корзина"}/>
       {cart.length ? (
@@ -67,6 +90,9 @@ const Cart = ({cart}) => {
                                price={item.price}/>
             })}
           </ul>
+          <div className="cart__comment">
+            <TextArea placeholder="Комментарий к заказу" allowClear onChange={textAreaChange}  />
+          </div>
           <div className={s.cart__footer}>
             <div className={s.cart__footer_left}>
               <div className={s.cart__results}>
@@ -79,11 +105,10 @@ const Cart = ({cart}) => {
                   className={s.cart__available}>{available ? 'В наличии' : 'По данном району продукция закончилась, выберите другой'}</span>
               ) : ''}
               {cart.length && currentUser?.id && available ? (
-                <button onClick={sendOrderData} className={s.cart__button_order}>Оформить заказ</button>
+                <button onClick={showModal} className={s.cart__button_order}>Продолжить</button>
               ) : null}
             </div>
-            <div className={s.cart__footer_select}>
-              <span className={s.cart__footer_select_title}>Район самовывоза:</span>
+            <div className={s.cart__footer_right}>
               <Select
                 className="cart__footer_select_ant"
                 popupClassName="cart__footer_select_popup"
@@ -109,6 +134,44 @@ const Cart = ({cart}) => {
         </div>
       )}
     </div>
+      <Modal title="Ваш заказ" open={isModalOpen} onOk={handleOk} onCancel={handleCancel} cancelText={'Отмена'} okText={'Оформить заказ'}>
+        <ul className={s.order__items}>
+          {cart.map((item) => {
+            return (
+              <li key={item.title} className={s.order__item}>
+                <div className={s.order__item_body}>
+                  <img className={s.order__item_photo} src={`${SITE}api/image/stickers/${item.photo}`} alt={item.title}/>
+                  <div className={s.order__item_info}>
+                    <h5 className={s.order__item_title}>{item.title}</h5>
+                    <div className={s.order__item_count}>
+                      <span>{item.count}&nbsp;шт</span>
+                      -
+                      <p>{item.price}&nbsp;₽</p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            )
+          })}
+        </ul>
+        <div className={s.order__footer}>
+          <div className={s.cart__results}>
+            <div className={s.cart__total_count}>{getTotalSumCart(cart).totalCount}&nbsp;шт</div>
+            <div className={s.cart__total_sum}><span>Итого:</span>&nbsp;{getTotalSumCart(cart).totalSum}&nbsp;₽
+            </div>
+          </div>
+          <div className={s.order__phone}>
+            <InputMask
+              className='input__phone'
+              placeholder='Телефон для связи'
+              mask='(+7) 999 999 99 99'
+              value={phone}
+              onChange={handleInputPhone}>
+            </InputMask>
+          </div>
+        </div>
+      </Modal>
+    </>
   );
 };
 
