ef6f533c3a032d0eb4108488d241687491433fad
Alexey <aaa@mworx.ru>
2024-05-15 11:33:40 +0300

    userlist admin page

--- src/components/Pages/UserList/UserList.jsx
+++ src/components/Pages/UserList/UserList.jsx
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import {getUsersData, postUsersUpdatedData} from "../../../utils/usersUtils";
-import {Watermark, Modal} from "antd";
+import {Watermark, Modal, Image} from "antd";
 import Header from "../../Header/Header";
 import {FormOutlined} from '@ant-design/icons';
+import {SITE} from "../../../utils/consts";
 
 const UserList = () => {
 
@@ -59,15 +60,42 @@ const UserList = () => {
                 return (
                   <li key={user.id} className="admin-users__item admin-user-item">
                     <div className="admin-user-item__body">
-                      {Object.entries(user).map((data, index) => {
-                        return (
-                          <div key={index} className="admin-user-item__row">
-                            <div className="admin-user-item__key">{data[0]}</div>
-                            <div style={data[1] !== '' ? { marginRight: '15px' } : {}} className="admin-user-item__value">{data[1]}</div>
-                            {data[0] !== 'chatId' && data[0] !== 'id' && <div onClick={() => showModal(Number(user.chatId), data[0], data[1])} className="admin-user-item__edit"><FormOutlined /></div>}
-                          </div>
-                        )
-                      })}
+                      <div className="admin-user-item__info">
+                        {Object.entries(user).map((data, index) => {
+                          return (
+                            <div key={index} className="admin-user-item__row">
+                              <div className="admin-user-item__key">{data[0]}</div>
+                              <div style={data[1] !== '' ? {marginRight: '15px'} : {}}
+                                   className="admin-user-item__value">{data[1]}</div>
+                              {data[0] !== 'chatId' && data[0] !== 'id' &&
+                                <div onClick={() => showModal(Number(user.chatId), data[0], data[1])}
+                                     className="admin-user-item__edit"><FormOutlined/></div>}
+                            </div>
+                          )
+                        })}
+                      </div>
+                      <div className="admin-user-item__image">
+                        {/*<img src={`${SITE}api/image/small/${user.carImage}_small.jpeg`} alt=""/>*/}
+                        <Image
+                          preview={{
+                            src: SITE + "api/image/" + user.carImage,
+                            mask: 'Просмотр',
+                            toolbarRender: () => null,
+                          }}
+                          // style={{
+                          //   objectFit: "cover",
+                          //   height: "150px",
+                          //   maxWidth: "150px",
+                          // }}
+                          loading="lazy"
+                          key={user.chatId}
+                          onError={(e) => {
+                            e.target.src = `${SITE}api/icons/not_found.png`
+                          }}
+                          src={`${SITE}api/image/small/${user.carImage}_small.jpeg`}
+                          alt={`${user.carbrand} ${user.carModel}`}
+                        />
+                      </div>
                     </div>
                   </li>
                 )
@@ -76,7 +104,9 @@ const UserList = () => {
           </ul>
         </div>
       </div>
-      <Modal title={`Столбец - ${modalData.column}`} wrapClassName={'admin-users__modal'} destroyOnClose={true} centered={true} open={isModalOpen} onOk={() => handleSave(modalData.chatId, modalData.column, modalInput)} okText={'Сохранить'} onCancel={handleCancel} cancelText={'Отмена'}>
+      <Modal title={`Столбец - ${modalData.column}`} wrapClassName={'admin-users__modal'} destroyOnClose={true}
+             centered={true} open={isModalOpen} onOk={() => handleSave(modalData.chatId, modalData.column, modalInput)}
+             okText={'Сохранить'} onCancel={handleCancel} cancelText={'Отмена'}>
         <div className="admin-users__modal-current-value">Текущее значение: {modalData.value}</div>
         <div className="admin-users__modal-new-value">
           <span>Новое значение:</span>
